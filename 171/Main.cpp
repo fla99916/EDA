16afb650c4fe4a8cda82a7eb2895681ecd1b2a21
fla99916 <fla99916@gmail.com>
2018-09-25 14:06:03 +0200

    Add files via upload

---
+++ 171/Main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	while (n != 0) {
+		int m[100000];
+		for (int i = 0; i < n; i++)
+		{
+			cin >> m[i];
+		}
+
+		int count = 1;
+		int highest = m[n - 1];
+		for (int j = n-2; j >= 0; j--)
+		{
+			if (m[j] > highest) {
+				count++;
+				highest = m[j];
+			}
+		}
+
+		cout << count << endl;
+
+		cin >> n;
+	}
+
+	return 0;
+}
