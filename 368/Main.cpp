d9a84910a1a366912963fb0a7ef7bb0c946de744
fla99916 <fla99916@gmail.com>
2018-09-25 13:13:11 +0200

    Add files via upload

---
+++ 368/Main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int h, c;
+
+	cin >> h >> c;
+	while (h != 0 && c != 0) {
+		if (h <= c) cout << 10 << endl;
+		else {
+			int t = h / c;
+			if(h%c == 0) cout << (t * 10) << endl;
+			else cout << (t * 10) + 10 << endl;
+		}
+
+		cin >> h >> c;
+	}
+
+	return 0;
+}
